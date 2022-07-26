61e721527f29a24b8a41f9a46cbe19b8fe4cad5a
Puneet Rawat <puneet@epigraph.us>
2022-07-26 18:41:01 +0530

    updated with global variants

--- scripts/index.js
+++ scripts/index.js
@@ -28,7 +28,21 @@ function main() {
                 const configID = loadconfig3.getAttribute("config-id");
                 EPIGRAPH_CONFIGURATOR_WC.api.core.loadSceneFromConfigurationID(configID);
             });
-            
+
+            const redVariantBtn = document.getElementById("redVariantBtn");
+            redVariantBtn.addEventListener('click', () => {
+                const lookCategory = redVariantBtn.getAttribute("look-category");
+                const lookVariant = redVariantBtn.getAttribute("look-variant");
+                EPIGRAPH_CONFIGURATOR_WC.api.core.switchGlobalVariant(lookCategory, lookVariant);
+            });
+
+            const blackVariantBtn = document.getElementById("blackVariantBtn");
+            blackVariantBtn.addEventListener('click', () => {
+                const lookCategory = blackVariantBtn.getAttribute("look-category");
+                const lookVariant = blackVariantBtn.getAttribute("look-variant");
+                EPIGRAPH_CONFIGURATOR_WC.api.core.switchGlobalVariant(lookCategory, lookVariant);
+            });
+
         }
     );
 
