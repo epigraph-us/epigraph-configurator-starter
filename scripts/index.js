7d8e6bba477b19741642440c56059f700bd73090
tanaydimri <tanay.dim@gmail.com>
2022-09-01 22:38:39 +0530

    Added a bare minimum UI for Floyd to begin inspecting the setup that we are proposing

--- scripts/index.js
+++ scripts/index.js
@@ -1,49 +1,121 @@
-var api_ready = false;
+var EPIGRAPH_CORE_API = undefined; // Storing a reference to the API directly.
+
+const CONFIG_2_JSON = {
+    "structure": [
+        {
+            "skuID": "Root",
+            "guid": "d095c7f6-78fc-4d31-9bfe-1e8c2b5b08ee",
+            "isEndNode": false,
+            "plugs": {
+                "root": {
+                    "guid": "228c897e-0cb1-43d4-86ed-23488adb77bc",
+                    "isOpen": false,
+                    "attachedPlugGUID": "2152f753-3a6e-4a2b-8704-2f083bc0405b",
+                    "isReceiverPlug": true
+                }
+            },
+            "overrideVariant": null
+        },
+        {
+            "skuID": "king_base",
+            "guid": "180b5c79-4991-48cf-baa3-8ec9b742f26a",
+            "isEndNode": false,
+            "plugs": {
+                "originplug_001": {
+                    "guid": "2152f753-3a6e-4a2b-8704-2f083bc0405b",
+                    "isOpen": false,
+                    "attachedPlugGUID": "228c897e-0cb1-43d4-86ed-23488adb77bc",
+                    "isReceiverPlug": false
+                },
+                "bed_hardware_plug": {
+                    "guid": "0e55497b-adb7-4c98-bafb-bdcfea291bb2",
+                    "isOpen": false,
+                    "attachedPlugGUID": "4751cfb2-0258-4672-a425-a6b1413f5b3d",
+                    "isReceiverPlug": true
+                }
+            },
+            "overrideVariant": null
+        },
+        {
+            "skuID": "king_hardware",
+            "guid": "207c42ea-1e5c-415a-9069-8edfd9273e0c",
+            "isEndNode": true,
+            "plugs": {
+                "hardware_bed_plug_001": {
+                    "guid": "4751cfb2-0258-4672-a425-a6b1413f5b3d",
+                    "isOpen": false,
+                    "attachedPlugGUID": "0e55497b-adb7-4c98-bafb-bdcfea291bb2",
+                    "isReceiverPlug": false
+                }
+            },
+            "overrideVariant": null
+        }
+    ],
+    "globalVariants": {
+        "wood": "walnut",
+        "hardware": "white"
+    }
+};
+
+
+/**
+ * A demonstration of how a saved configuration could be loaded from either:
+ * 1. A JSON object that is stored and managed by you.
+ * 2. An ID that Epigraph stores and fetches for your convenience.
+ */
+function setupPreConfigurationLoaderThumbnails() {
+    // Configuration 01 - Examples of being loaded from Epigraph's database.
+    // You would need to discuss this initially with Epigraph's dev team if you'd like to save and load configurations from our Database.
+    // The method being used is documented here: https://epg-configurator-client-docs-dot-composite-sun-338620.uc.r.appspot.com/api-docs/Classes/ConfiguratorCoreAPI/index.html#loadscenefromconfigurationididtoload
+    const loadconfig1 = document.getElementById("loadconfig1");
+    loadconfig1.addEventListener('click', () => {
+        const configID = loadconfig1.getAttribute("config-id");
+        EPIGRAPH_CORE_API.loadSceneFromConfigurationID(configID);
+    });
+
+    // Configuration 03 - Example of being loaded from a JSON object (In the first example, the same JSON object gets internally fetched from Epigraph's database).
+    // You are expected to store and fetch this JSON object from a database if you decide to move forward with this approach.
+    // The method being used is documented here: https://epg-configurator-client-docs-dot-composite-sun-338620.uc.r.appspot.com/api-docs/Classes/ConfiguratorCoreAPI/index.html#ConfiguratorCoreAPI+loadSceneFromConfiguration
+    const loadconfig3 = document.getElementById("loadconfig2");
+    loadconfig3.addEventListener('click', () => {
+        EPIGRAPH_CORE_API.loadSceneFromConfiguration(CONFIG_2_JSON);
+    });
+}
+
+
+function onColourVariantChange(event) {
+    const colourVariantElement = event.target;
+
+    const lookCategory = colourVariantElement.getAttribute("look-category");
+    const lookVariant = colourVariantElement.getAttribute("look-variant");
+    EPIGRAPH_CORE_API.switchGlobalVariant(lookCategory, lookVariant);
+}
+
+
+function setupColourVariantThumbnails() {
+    const allColourVariantThumbnails = [...document.getElementsByClassName("colour-thumbnail")];
+    for (const colorThumbnail of allColourVariantThumbnails) {
+        colorThumbnail.addEventListener("click", onColourVariantChange);
+    }
+}
+
 
 function main() {
     var EPIGRAPH_CONFIGURATOR_WC = document.getElementById("wcEpigraphConfigurator");
 
     EPIGRAPH_CONFIGURATOR_WC.addEventListener(
-        "core-app-connected",
+        "coreApi:ready",
         function (event) {
             console.log("Core API successfully initialised.");
-            api_ready = true;
-            /** Start setting up the behaviour of the website 
-             * if the configurator was successfully initialised 
-             * without any errors
-             */
-            const loadconfig1 = document.getElementById("loadconfig1");
-            loadconfig1.addEventListener('click', () => {
-                const configID = loadconfig1.getAttribute("config-id");
-                EPIGRAPH_CONFIGURATOR_WC.api.core.loadSceneFromConfigurationID(configID);
-            });
+            EPIGRAPH_CORE_API = EPIGRAPH_CONFIGURATOR_WC.api.core;
 
-            const loadconfig2 = document.getElementById("loadconfig2");
-            loadconfig2.addEventListener('click', () => {
-                const configID = loadconfig2.getAttribute("config-id");
-                EPIGRAPH_CONFIGURATOR_WC.api.core.loadSceneFromConfigurationID(configID);
-            });
+            /** 
+             * Setting up the preConfigured scene loading thumbnails.
+            */
+            setupPreConfigurationLoaderThumbnails();
 
-            const loadconfig3 = document.getElementById("loadconfig3");
-            loadconfig3.addEventListener('click', () => {
-                const configID = loadconfig3.getAttribute("config-id");
-                EPIGRAPH_CONFIGURATOR_WC.api.core.loadSceneFromConfigurationID(configID);
-            });
-
-            const redVariantBtn = document.getElementById("redVariantBtn");
-            redVariantBtn.addEventListener('click', () => {
-                const lookCategory = redVariantBtn.getAttribute("look-category");
-                const lookVariant = redVariantBtn.getAttribute("look-variant");
-                EPIGRAPH_CONFIGURATOR_WC.api.core.switchGlobalVariant(lookCategory, lookVariant);
-            });
-
-            const blackVariantBtn = document.getElementById("blackVariantBtn");
-            blackVariantBtn.addEventListener('click', () => {
-                const lookCategory = blackVariantBtn.getAttribute("look-category");
-                const lookVariant = blackVariantBtn.getAttribute("look-variant");
-                EPIGRAPH_CONFIGURATOR_WC.api.core.switchGlobalVariant(lookCategory, lookVariant);
-            });
-
+            // Setting up the Colour Variant Switcher.
+            setupColourVariantThumbnails();
         }
     );
 
@@ -51,7 +123,8 @@ function main() {
         "coreApi:failed",
         function (event) {
             console.error("Failed to initialise core api.");
-            api_ready = false;
+            console.error(event);
+
             /** Gracefully exit and probably show a modal to the user 
              * with a proper reasoning for the API to not initialise. 
              * This could be a hardware limitation too and 
---
+++ scripts/preconfigurations/config2.js
@@ -0,0 +1,109 @@
+export const config2 = {
+    "structure": [
+        {
+            "skuID": "Root",
+            "guid": "18c0326a-5c0e-442c-94da-dd1958fde870",
+            "isEndNode": false,
+            "plugs": {
+                "root": {
+                    "guid": "415c24ab-cfcb-4847-9f41-51ee25d1fdbf",
+                    "isOpen": false,
+                    "attachedPlugGUID": "486584e0-a712-4da6-b70f-e34c26ab4523",
+                    "isReceiverPlug": true
+                }
+            },
+            "overrideVariant": null
+        },
+        {
+            "skuID": "Craftsman68",
+            "guid": "b730f290-49f2-4829-b67c-81fcebb3876e",
+            "isEndNode": false,
+            "plugs": {
+                "plug001": {
+                    "guid": "486584e0-a712-4da6-b70f-e34c26ab4523",
+                    "isOpen": false,
+                    "attachedPlugGUID": "415c24ab-cfcb-4847-9f41-51ee25d1fdbf",
+                    "isReceiverPlug": false
+                },
+                "plug004": {
+                    "guid": "f479224e-3596-470f-84c3-84aac565c0a4",
+                    "isOpen": false,
+                    "attachedPlugGUID": "d59e0301-2cde-42d5-8f18-3319778edec1",
+                    "isReceiverPlug": true
+                }
+            },
+            "overrideVariant": null
+        },
+        {
+            "skuID": "Craftsman70",
+            "guid": "ebe5bc63-0b64-46da-8b57-510867f13026",
+            "isEndNode": false,
+            "plugs": {
+                "plug002": {
+                    "guid": "ee190d26-673f-490b-b60f-2a7ffddebbbb",
+                    "isOpen": false,
+                    "attachedPlugGUID": "870c6134-f731-42c7-89cd-f24639828359",
+                    "isReceiverPlug": true
+                },
+                "plug003": {
+                    "guid": "d59e0301-2cde-42d5-8f18-3319778edec1",
+                    "isOpen": false,
+                    "attachedPlugGUID": "f479224e-3596-470f-84c3-84aac565c0a4",
+                    "isReceiverPlug": false
+                }
+            },
+            "overrideVariant": null
+        },
+        {
+            "skuID": "Pivotlight",
+            "guid": "f9942f03-121e-402e-aafe-5613bef4ad38",
+            "isEndNode": true,
+            "plugs": {
+                "plug001": {
+                    "guid": "ef681188-e33e-48d2-a7b2-76d03d726746",
+                    "isOpen": false,
+                    "attachedPlugGUID": "7cdbb879-ccaf-4aaa-b337-040095315e3f",
+                    "isReceiverPlug": false
+                }
+            },
+            "overrideVariant": null
+        },
+        {
+            "skuID": "SpraycanShelf",
+            "guid": "70d71643-4851-49ee-b2f1-5e4d632c572d",
+            "isEndNode": true,
+            "plugs": {
+                "plug001": {
+                    "guid": "5455ae3a-da5a-4ade-b89d-1558dfdc0bd9",
+                    "isOpen": false,
+                    "attachedPlugGUID": "0f8fd21b-bf36-4b19-9cf4-215f29284bac",
+                    "isReceiverPlug": false
+                }
+            },
+            "overrideVariant": null
+        },
+        {
+            "skuID": "Craftsman67",
+            "guid": "79bcf6b4-6523-463c-9c2d-0cacd83ff880",
+            "isEndNode": false,
+            "plugs": {
+                "in_1": {
+                    "guid": "0f8fd21b-bf36-4b19-9cf4-215f29284bac",
+                    "isOpen": false,
+                    "attachedPlugGUID": "5455ae3a-da5a-4ade-b89d-1558dfdc0bd9",
+                    "isReceiverPlug": true
+                },
+                "in_3": {
+                    "guid": "7cdbb879-ccaf-4aaa-b337-040095315e3f",
+                    "isOpen": false,
+                    "attachedPlugGUID": "ef681188-e33e-48d2-a7b2-76d03d726746",
+                    "isReceiverPlug": true
+                }
+            },
+            "overrideVariant": null
+        }
+    ],
+    "globalVariants": {
+        "exterior": "red"
+    }
+};
